4a0b14176e2793f0a2a47de127d91d3ee6da191e
Boris Adryan <boris@Boris-Adryans-iMac.local>
2015-08-21 16:12:06 +0100

    fixed

--- deps.sh
+++ deps.sh
@@ -1,10 +1,7 @@
  #!/bin/bash
 set -o errexit
-apt-get update
-apt-get install minicom
 wget http://node-arm.herokuapp.com/node_latest_armhf.deb
 dpkg -i node_latest_armhf.deb
-npm cache clean
 npm install -g --unsafe-perm node-red
 cd /usr/local/lib/node_modules/node-red/
 npm install easy-crc32
